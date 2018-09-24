e8419cd7619563104c2708edaa2b94c0c002cc68
ignacio bochatay <ignaciobochatay@gmail.com>
2018-09-24 12:10:22 -0300

    adding event handler to StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+
+  myInput= React.createRef();
+
+  goToStore = (event) => {
+    //evita al form de realizar el submit
+    event.preventDefault();
+    //obtener el texto del input
+    const storeName = this.myInput.value.value;
+    //cambiar la ruta con React Router a /store/Id
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input 
+          type="text"
+          ref={this.myInput} 
+          required 
+          placeholder="Store Name" 
+          defaultValue={getFunName()} 
+        />
         <button type="submit">Visit Store</button>
       </form>
     )
