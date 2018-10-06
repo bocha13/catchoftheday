a447913074f336505acab262dcee0ff4e2ad1faf
ignacio bochatay <ignaciobochatay@gmail.com>
2018-10-05 21:38:47 -0300

    adding state and setState

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    //1- copiar el state actual
+    const fishes = {...this.state.fishes};
+    //2- agregar fish nuevo a la variable
+    fishes[`fish${Date.now()}`] = fish;
+    //3- pasar el nuevo objeto fish al estado
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
          <Header tagline="Wes is cool"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
         
       </div>
     )
